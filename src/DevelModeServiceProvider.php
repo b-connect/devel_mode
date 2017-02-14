7b440cca48a883c1d1ad92229247d8eb2e4509da
Erik Seifert <erik.seifert@b-connect.de>
2017-02-14 10:44:57 +0100

    Remove unused use statement

--- src/DevelModeServiceProvider.php
+++ src/DevelModeServiceProvider.php
@@ -4,7 +4,6 @@ namespace Drupal\devel_mode;
 
 use \Drupal\Core\DependencyInjection\ServiceProviderBase;
 use \Drupal\Core\DependencyInjection\ContainerBuilder;
-use \Symfony\Component\DependencyInjection\Definition;
 
 /**
  * Overrides the class for the menu link tree.
